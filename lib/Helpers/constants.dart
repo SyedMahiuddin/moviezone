8041df46bab65db10c0c81eabf3b0fd7e371c443
syedmahiuddin <syedmahiuddin01@gmail.com>
2024-09-02 14:36:15 +0600

    basic structure

---
+++ lib/Helpers/constants.dart
@@ -0,0 +1,5 @@
+class AppConstant{
+  final String apiKey = '9b4ced201079b3f778dd56e92ceca7a4';
+  final String url = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+  static String mapApiKey = 'AIzaSyC7S8muAv_llhZLKdz-RT6133LS5TnxcLA';
+}
---
+++ lib/Views/bottom_navigation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
